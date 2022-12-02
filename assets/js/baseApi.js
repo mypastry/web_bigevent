3926d7600c02034a4d78b69e0ec1cb70b723797c
irene <893324012@qq.com>
2022-12-02 16:18:38 +0800

    完成了主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,6 +2,22 @@ $.ajaxPrefilter(function(options){
     console.log(options.url);
 
     options.url = 'http://www.liulongbin.top:3007'+options.url
+    if(options.url.indexOf('/my/' !== -1)){
+        options.headers ={
+            Authorization:localStorage.getItem('token')||''
+    }
     
+    }
+    options.complete = function(res){
+        console.log(res);
+        
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            console.log(111);
+            
+            localStorage.removeItem('token')
+            window.location.href='/login.html'
+        }
+    }
+
+
 })
-    
